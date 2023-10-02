7ca9f4bb0fe87254e0940f7329407519b380172d
“Eltion <ebehrami01@gmail.com>
2023-10-02 15:23:42 -0400

    beginning to work on views

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController 
+
+    def new 
+        @user = User.new 
+        render :new
+    end 
+
+    def create 
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        session[:session_token] = @user.reset_session_token! 
+        redirect_to cats_url
+    end 
+
+
+    def destroy
+
+    end 
+
+
+
+
+
+end 
---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -0,0 +1,46 @@
+class User < ApplicationRecord 
+    validates :username, :password_digest, :session_token, presence: true 
+    validates :username, :session_token, uniqueness: true 
+
+    before_validation :ensure_session_token 
+    attr_reader :password 
+
+    def ensure_session_token 
+        self.session_token ||= self.generate_unique_session_token
+    end 
+
+    def password=(password)
+        @password = password 
+
+        password_digest = BCrypt::Password.create(password)
+    end 
+
+    def is_password?(password) 
+        password_object = BCrypt::Password.new(password_digest)
+        password_object.is_password?(password)
+    end 
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user 
+        else 
+            nil
+        end 
+    end 
+
+    def reset_session_token!
+        session_token = self.generate_unique_session_token
+        session_token.save! 
+        session_token 
+    end 
+
+    private 
+
+    def generate_unique_session_token
+        SecureRandom::urlsafe_base64
+    end 
+
+
+
+end 
